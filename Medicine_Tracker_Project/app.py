a458bf7144cfee0583aa648838375aeeaa4b1098
sanskrutibhosale2006-max <sanskrutibhosale2006@gmail.com>
2026-05-11 13:09:46 +0530

    file uploaded

---
+++ Medicine_Tracker_Project/app.py
@@ -0,0 +1,294 @@
+from flask import Flask, render_template, request, redirect, session
+from medicine_inventory import get_status
+from database import connect_db, create_table
+
+app = Flask(__name__)
+
+app.secret_key = "medicine_secret_key"
+
+create_table()
+
+
+# LOGIN PAGE
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = connect_db()
+
+        user = conn.execute(
+
+            '''
+
+            SELECT * FROM users
+
+            WHERE username = ? AND password = ?
+
+            ''',
+
+            (username, password)
+
+        ).fetchone()
+
+        conn.close()
+
+        if user:
+
+            session['user'] = username
+
+            return redirect('/')
+
+        else:
+
+            return render_template(
+                'login.html',
+                error="Invalid Username or Password"
+            )
+
+    return render_template('login.html')
+
+
+# REGISTER PAGE
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = connect_db()
+
+        try:
+
+            conn.execute(
+
+                '''
+
+                INSERT INTO users (username, password)
+
+                VALUES (?, ?)
+
+                ''',
+
+                (username, password)
+
+            )
+
+            conn.commit()
+
+            conn.close()
+
+            return redirect('/login')
+
+        except:
+
+            conn.close()
+
+            return render_template(
+                'register.html',
+                error="Username already exists"
+            )
+
+    return render_template('register.html')
+
+
+# LOGOUT
+@app.route('/logout')
+def logout():
+
+    session.pop('user', None)
+
+    return redirect('/login')
+
+
+# HOME PAGE
+@app.route('/')
+def home():
+
+    if 'user' not in session:
+        return redirect('/login')
+
+    search = request.args.get('search', '')
+
+    conn = connect_db()
+
+    if search:
+
+        medicines = conn.execute(
+
+            '''
+
+            SELECT * FROM medicines
+
+            WHERE name LIKE ?
+
+            ''',
+
+            ('%' + search + '%',)
+
+        ).fetchall()
+
+    else:
+
+        medicines = conn.execute(
+            'SELECT * FROM medicines'
+        ).fetchall()
+
+    conn.close()
+
+    inventory = []
+
+    expired_count = 0
+    low_stock_count = 0
+    expiring_soon_count = 0
+
+    for item in medicines:
+
+        medicine = dict(item)
+
+        medicine['status'] = get_status(medicine)
+
+        if "EXPIRED" in medicine['status']:
+            expired_count += 1
+
+        elif "LOW STOCK" in medicine['status']:
+            low_stock_count += 1
+
+        elif "Expiring Soon" in medicine['status']:
+            expiring_soon_count += 1
+
+        inventory.append(medicine)
+
+    total_medicines = len(inventory)
+
+    return render_template(
+
+        'index.html',
+
+        inventory=inventory,
+
+        total_medicines=total_medicines,
+
+        expired_count=expired_count,
+
+        low_stock_count=low_stock_count,
+
+        expiring_soon_count=expiring_soon_count,
+
+        search=search,
+
+        username=session['user']
+
+    )
+
+
+# ADD MEDICINE
+@app.route('/add', methods=['GET', 'POST'])
+def add_medicine():
+
+    if 'user' not in session:
+        return redirect('/login')
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        stock = request.form['stock']
+        expiry = request.form['expiry']
+
+        conn = connect_db()
+
+        conn.execute(
+
+            'INSERT INTO medicines (name, stock, expiry) VALUES (?, ?, ?)',
+
+            (name, stock, expiry)
+
+        )
+
+        conn.commit()
+
+        conn.close()
+
+        return redirect('/')
+
+    return render_template('add_medicine.html')
+
+
+# DELETE MEDICINE
+@app.route('/delete/<int:id>')
+def delete_medicine(id):
+
+    if 'user' not in session:
+        return redirect('/login')
+
+    conn = connect_db()
+
+    conn.execute(
+        'DELETE FROM medicines WHERE id = ?',
+        (id,)
+    )
+
+    conn.commit()
+
+    conn.close()
+
+    return redirect('/')
+
+
+# UPDATE MEDICINE
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update_medicine(id):
+
+    if 'user' not in session:
+        return redirect('/login')
+
+    conn = connect_db()
+
+    medicine = conn.execute(
+        'SELECT * FROM medicines WHERE id = ?',
+        (id,)
+    ).fetchone()
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        stock = request.form['stock']
+        expiry = request.form['expiry']
+
+        conn.execute(
+
+            '''
+
+            UPDATE medicines
+
+            SET name = ?, stock = ?, expiry = ?
+
+            WHERE id = ?
+
+            ''',
+
+            (name, stock, expiry, id)
+
+        )
+
+        conn.commit()
+
+        conn.close()
+
+        return redirect('/')
+
+    conn.close()
+
+    return render_template(
+        'update_medicine.html',
+        medicine=medicine
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
